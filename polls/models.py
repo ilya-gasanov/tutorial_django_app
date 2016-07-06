2ed6b23f5dbced4f89f040c3fe436ae0923de04d
Ilya Gasanov <ilya-gasanov@yandex.ru>
2016-07-06 19:56:15 +0300

    makemigrations polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,13 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField('question text', max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField('choice text', max_length=200)
+    votes = models.IntegerField(default=0)
